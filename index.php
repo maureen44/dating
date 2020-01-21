309323658775f2ee2f5f0b5fb3f0e92ce904843c
Maureen Kariuki <mkariuki2@mail.greenriver.edu>
2020-01-21 02:22:04 -0500

    Added an image to the images folder, added code in the home.html file, added to style.css, and added header commet to index.php file

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-//This is our Controller!
+/**
+ * Maureen Kariuki
+ * 01/20/2020
+ *
+ * This is the controller
+ * The base class is instantiated,
+ * fat free route declared and fat free
+ * is run.
+ */
+
 //Turn on error reporting -- this is critical
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -8,14 +17,16 @@ error_reporting(E_ALL);
 require ("vendor/autoload.php");
 
 //instantiate Fat-free
-$f3 = Base:: instance();
+$f3 = Base::instance();
 
 //default route
 $f3->route('GET /', function ()
 {
     $view = new Template();
     echo $view->render('views/home.html');
+   // echo "Cake Lovers Dating!";
 });
 
 //run fat free
-$f3-> run();
+$f3->run();
+?>
